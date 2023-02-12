7382468a7f040c37a17977b3857f6b805adaa696
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-12 16:29:52 +0300

    allnewdice

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import "./App.css";
 import Die from "./Die";
 
 function App() {
+
+  function allNewDice() {
+    const arr = [];
+    for (let i = 0; i < 10; i++){
+      arr.push(Math.floor(Math.random() * (7 - 1) + 1));
+    }
+    console.log(arr);
+  }
+
+  allNewDice()
   return (
     <div className="app">
       <main>
