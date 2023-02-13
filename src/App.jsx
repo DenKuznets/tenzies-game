5b8d9a7eeba0caa954dd55441c7913e681bf1c14
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-13 17:44:53 +0300

    win implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Die from "./Die";
 import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = useState(() => allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+  useEffect(() => {
+    let value = dice[0].value;
+    let win = dice.every((die) => die.value === value && die.isHeld);
+    if (win) {
+      setTenzies(true);
+      console.log("You won!");
+    }
+  }, [dice]);
 
   function allNewDice() {
     const arr = [];
@@ -29,19 +38,22 @@ function App() {
   ));
 
   function rollDice() {
-    setDice(prev => prev.map(die => {
-      let newDie = { ...die };
-      // если кубик не отложен, перекинуть его (поменять значение)
-      if(!newDie.isHeld) newDie.value = Math.floor(Math.random() * (7 - 1) + 1);
-      return newDie;
-    }));
+    setDice((prev) =>
+      prev.map((die) => {
+        let newDie = { ...die };
+        // если кубик не отложен, перекинуть его (поменять значение)
+        if (!newDie.isHeld)
+          newDie.value = Math.floor(Math.random() * (7 - 1) + 1);
+        return newDie;
+      })
+    );
   }
 
   function holdDice(id) {
-    console.log(id);
+    // console.log(id);
     setDice((prev) =>
       prev.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;        
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
       })
     );
   }
