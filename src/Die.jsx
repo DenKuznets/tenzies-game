ec66d189d93836c5b6900a0520db5cfd800816e9
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-14 15:35:51 +0300

    adding dots

--- src/Die.jsx
+++ src/Die.jsx
@@ -2,12 +2,16 @@ import "./Die.css";
 
 export default function Die(props) {
   let heldClass = props.isHeld ? "green-bg" : "";
+  let dots = [];
+  for (let i = 0; i < props.value; i++) {
+    dots.push(<div className="dot"></div>);
+  }
   return (
-    <div
-      onClick={props.onClick}
-      className={`die ${heldClass}`}
-    >
-      {props.value}
+    <div onClick={props.onClick} className={`die ${heldClass}`}>
+      {/* {props.value} */}
+      <div className={`dots${props.value}`}>
+        {dots}
+      </div>
     </div>
   );
 }
