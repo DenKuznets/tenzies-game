f9a865802cdde984ae8c0e2432c0497628681ea3
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-15 20:27:47 +0300

    текст

--- src/local.js
+++ src/local.js
@@ -2,7 +2,7 @@ export const text = {
   rus: {
     newGame: "Новая Игра",
     rollBtn: "Бросок",
-    score: "Результат:",
+    score: "Всего бросков:",
     descr: "Кидайте кубики, пока не выбросите одинаковые значения на всех. Клик по кубику сохраняет результат.",
     press: "Жамите",
     toStart: "чтобы начать",
@@ -12,7 +12,7 @@ export const text = {
   eng: {
     newGame: "New Game",
     rollBtn: "Roll",
-    score: "Your score:",
+    score: "Total rolls:",
     descr: "Roll until all dice are the same. Click each die to freeze it at its current value between rolls.",
     press: "Press",
     toStart: "to start",
